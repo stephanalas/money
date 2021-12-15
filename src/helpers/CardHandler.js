d8da9fbe334868904afebed95a149ec60a0426c5
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-12-14 22:19:48 -0500

    working on dealCard function

--- src/helpers/CardHandler.js
+++ src/helpers/CardHandler.js
@@ -2,9 +2,9 @@ import Card from '../classes/Card';
 
 export default class CardHandler {
   constructor(scene) {
+    this.scene = scene;
     this.createDeck = () => {
       const suits = ['diamonds', 'club', 'spade', 'heart', 'joker'];
-
       const rank = [
         '2',
         '3',
@@ -64,7 +64,6 @@ export default class CardHandler {
           suitIdx++;
         }
         let currentRank = rank[rankIdx];
-        console.log(rankIdx);
         let parsedRank = parseInt(currentRank);
         // if it is not a face card
         if (!isNaN(parsedRank)) {
@@ -94,5 +93,46 @@ export default class CardHandler {
       }
       return deck;
     };
+    this.shuffleDeck = this.shuffleDeck.bind(this);
+    this.dealCards = this.dealCards.bind(this);
   }
+  shuffleDeck = (deck) => {
+    return Phaser.Utils.Array.Shuffle(deck);
+  };
+  dealCards(deck, player, findMoney = false) {
+    const card = deck.pop();
+    const numberOfCards = card.cardsDealt;
+    const hand = player.hand;
+    const { height, width } = this.scene.game.scale;
+    // hand.add(card);
+    const playersCards = [card];
+    for (let i = 0; i < numberOfCards; i++) {
+      playersCards.push(deck.pop());
+    }
+    let x = width / 2,
+      y = height / 2,
+      offset = 0;
+    const tweens = playersCards.map((card, idx) => {
+      x += 32;
+      card.setDepth(idx);
+      return {
+        x,
+        y,
+        targets: card,
+        ease: 'Power2',
+        completeDelay: !idx ? 500 : 0,
+      };
+    });
+
+    this.scene.tweens.timeline({
+      tweens,
+      totalDuration: 3000,
+    });
+  }
+  // tween to player dealt area || middle of game
+  // pause to see cardDealt amount
+  // onComplete add card to player hand group
+  // deal more cards to player dealt area dependin gon cardsDealt amount
+  // if its the last player
+  // the last card decides money
 }
